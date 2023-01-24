2cfae271671152c66418809e7c97b924cbe678c7
sofia.weckerlin24 <sofia.weckerlin@gmail.com>
2023-01-24 11:24:59 -0600

    added another print statement

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.awt.*;
 public class Main{
     public static void main(String[] args){
       System.out.println("I'm on replit!");
+        System.out.println("Now I'm on IntelliJ!");
         JFrame myFrame = new JFrame();
         myFrame.setSize(500, 500);
         Main myPanel = new Main();
