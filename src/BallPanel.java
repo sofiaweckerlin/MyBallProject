8bac747eaa585cde588c0959b26729f4c81ae5a3
sofia.weckerlin24 <sofia.weckerlin@gmail.com>
2023-01-26 11:28:45 -0600

    move ball right

--- src/BallPanel.java
+++ src/BallPanel.java
@@ -7,45 +7,48 @@ import java.sql.SQLOutput;
 public class BallPanel extends JPanel{
     
     ArrayList<Ball> balls = new ArrayList<Ball>();
+    Ball cat;
     public BallPanel(){
         setBackground(Color.RED);
+        cat = new Ball(this);
         for(int i = 0; i<20; i++){
           Ball dog = new Ball(this);
           balls.add(dog);
         }
-        // setFocusable(true);
-        // addKeyListener(new KeyListener() {
-        //     @Override
-        //     public void keyTyped(KeyEvent e) {
+         setFocusable(true);
+         addKeyListener(new KeyListener() {
+             @Override
+             public void keyTyped(KeyEvent e) {
 
-        //     }
+             }
 
-        //     @Override
-        //     public void keyPressed(KeyEvent e) {
-        //         System.out.println(e.getKeyCode());
-        //         int keyCode = e.getKeyCode();
-        //         if(keyCode == 68){
-        //             balls.get(i).moveRight();
-        //         }
+             @Override
+             public void keyPressed(KeyEvent e) {
+                 System.out.println(e.getKeyCode());
+                 int keyCode = e.getKeyCode();
+                 if(keyCode == 39){
+                     cat.moveRight();
+                 }
 
-        //     }
+             }
 
-        //     @Override
-        //     public void keyReleased(KeyEvent e) {
+             @Override
+             public void keyReleased(KeyEvent e) {
 
-        //     }
-        // });
+             }
+         });
 
     }
 
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
+        cat.drawBall(g);
 
-        for(int i=0; i<20; i++){
-          balls.get(i).drawBall(g);
-          balls.get(i).moveBall(this);
-        }
+//        for(int i=0; i<20; i++){
+//          balls.get(i).drawBall(g);
+//          balls.get(i).moveBall(this);
+//        }
 
         try{
             Thread.sleep(10);
